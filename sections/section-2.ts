465714c560c9d661021b79512f770d4ae5a49702
branuxgt <bmiranda_g@outlook.com>
2025-03-04 21:44:11 -0600

    First commit

---
+++ sections/section-2.ts
@@ -0,0 +1,25 @@
+
+let numberOfLines = 1;
+
+export function printLineNumber(){
+    // numberOfLines = numberOfLines + 3;
+    console.log('Linea #', numberOfLines);
+    numberOfLines++;
+}
+
+
+printLineNumber();
+printLineNumber();
+printLineNumber();
+printLineNumber();
+
+/*
+console.log('Linea #', numberOfLines);
+
+increaseLineNumber();
+console.log('Linea #', numberOfLines);
+increaseLineNumber();
+console.log('Linea #', numberOfLines);
+increaseLineNumber();
+console.log('Linea #', numberOfLines);
+*/
